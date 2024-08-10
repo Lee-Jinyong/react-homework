102f62ba9349b8c3f1cfae9cd196c1eccbc240df
Lee-Jinyong <yjy1480@gmail.com>
2024-08-11 02:19:51 +0900

    mission03 노트 폼 업데이트 관리
    - 노트 작성 화면에서 제목, 내용, 작성자 입력 시 상태 업데이트

--- mission03/src/components/NoteForm.tsx
+++ mission03/src/components/NoteForm.tsx
@@ -1,8 +1,8 @@
-import { FormEvent, useId } from 'react';
+import { ChangeEvent, FormEvent, useId, useState } from 'react';
 import { getUserList } from '../api/getUser';
-import './NoteForm.css';
 import { NoteType } from '../types/note';
 import { convertHTMLToText } from '@/utils/convertTextToHTMLString';
+import './NoteForm.css';
 
 const userList = getUserList();
 
@@ -16,18 +16,40 @@ function NoteForm({ mode = 'create', note }: NoteForm) : JSX.Element {
   const contentId = useId();
   const userId = useId();
 
+  // 상태 선언
+  const [formData, setFormData] = useState({
+    title: '',
+    content: '',
+    userId: 0,
+  })
+
+  // 상태 업데이트
+
+  const handleUpdateFormData = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+    const { name, value } = e.target;
+
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  }
+
+  // 노트 생성
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
   };
 
+  // 노트 초기화
   const handleReset = (e: FormEvent) => {
     e.preventDefault();
   };
 
+  // 노트 삭제
   const handleDelete = () => {
     console.log('delete');
   };
 
+  // 파생 상태
   const isCreateMode = mode.includes('create');
   const submitButtonLabel = isCreateMode ? '추가' : '수정';
 
@@ -39,21 +61,34 @@ function NoteForm({ mode = 'create', note }: NoteForm) : JSX.Element {
     <form className="NoteForm" onSubmit={handleSubmit} onReset={handleReset}>
       <div className="formControl">
         <label htmlFor={titleId}>제목</label>
-        <input type="text" id={titleId} defaultValue={note?.title} />
+        <input
+          type="text"
+          id={titleId}
+          name="title"
+          value={note?.title}
+          onChange={handleUpdateFormData}
+        />
       </div>
 
       <div className="formControl">
         <label htmlFor={contentId}>내용</label>
         <textarea
           id={contentId}
-          defaultValue={note && convertHTMLToText(note.content)}
+          name="content"
+          value={note && convertHTMLToText(note.content)}
+          onChange={handleUpdateFormData}
         />
       </div>
 
       {isCreateMode && (
         <div className="formControl">
           <label htmlFor={userId}>작성자</label>
-          <select id={userId}>
+          <select
+            id={userId}
+            name="userId"
+            value={formData.userId}
+            onChange={handleUpdateFormData}
+          >
             <option>작성자 선택</option>
             {userList.map((user) => (
               <option key={user.id} value={user.id}>
