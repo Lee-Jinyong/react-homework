1954f969e196a33a8fd7066d292e8cf2a855309e
Lee-Jinyong <yjy1480@gmail.com>
2024-08-11 01:17:52 +0900

    mission03 뒤로가기 버튼 함수 연결
    - 뒤로가기 버튼을 누르면 노트 리스트 화면으로 돌아감

--- mission03/src/pages/NoteCreatePage.tsx
+++ mission03/src/pages/NoteCreatePage.tsx
@@ -1,11 +1,19 @@
+import { ROUTES } from '@/constants/routes';
 import BackLink from '../components/BackLink';
 import NoteForm from '../components/NoteForm';
 import './NoteCreatePage.css';
 
-function NoteCreatePage() {
+interface NoteCreatePage {
+  onChangeRoute : (nextRoute: string) => void
+}
+
+function NoteCreatePage({ onChangeRoute }: NoteCreatePage): JSX.Element {
+
+  const handleBackToList = () => onChangeRoute?.(ROUTES.list);
+
   return (
     <div className="NoteCreatePage">
-      <BackLink />
+      <BackLink onClick={handleBackToList}/>
       <NoteForm mode="create" />
     </div>
   );
