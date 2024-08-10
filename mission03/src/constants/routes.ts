75656772ceb22e9c38ec51c82fd28b56edf95271
Lee-Jinyong <yjy1480@gmail.com>
2024-08-11 01:02:49 +0900

    mission03 루트 타입 수정

--- mission03/src/constants/routes.ts
+++ mission03/src/constants/routes.ts
@@ -1,4 +1,11 @@
-export const ROUTES: { [key: string]: string } = {
+interface ROUTES_TYPE {
+  list: string;
+  create: string;
+  detail: string;
+  edit: string;
+}
+
+export const ROUTES: ROUTES_TYPE = {
   list: "list",
   create: "create",
   detail: "detail",
